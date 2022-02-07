4aee5606e9d18e65d0adbadc5a0ae3dfeca1b1f0
alexruizlul <alex007ruiz@gmail.com>
2022-02-07 08:31:17 -0600

    [DEPLOY] initial deploy to vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,38 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+			  'sanFrancisco': "url('sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('yosemite.jpg')",
+			  'LA': "url('LA.jpg')",
+			  'seattle': "url('seattle.jpg')",
+			  'new_york': "url('new_york.jpg')",
+			  'norway': "url('norway.jpg')",
+			  'sydney': "url('sydney.jpg')",
+			  'miami': "url('miami.jpg')",
+			  'switzerland': "url('switzerland.jpg')",
+			  'bali': "url('bali.jpg')",
+			  'norway': "url('norway.jpg')",
+			  'chicago': "url('chicago.jpg')",
+			  'europe': "url('europe.jpg')",
+			  'iceland': "url('iceland.jpg')",
+			  
+			},
+			backgroundColor: theme =>({
+				...theme('colors'),
+				'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			}),
+			textColor: theme => ({
+        ...theme('colors'),
+				'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			}),
+    },
   },
   plugins: [],
 }
